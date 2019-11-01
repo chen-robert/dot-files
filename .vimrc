4f7510f9c5bcd68c96d2c9775338dae6eb317048
Robert <robertchen@live.com>
2019-11-01 19:12:07 +0000

    Uploaded dot files

---
+++ .vimrc
@@ -0,0 +1,8 @@
+set expandtab
+set tabstop=2
+set softtabstop=2
+set shiftwidth=2
+
+set autoindent
+
+au BufNewFile,BufRead *.ejs set filetype=html
---
+++ init.sh
@@ -0,0 +1,15 @@
+rm /etc/update-motd.d/* 
+apt-get --assume-yes install screenfetch > /dev/null \
+  && cp 01-custom /etc/update-motd.d/ \
+  && chmod +x /etc/update-motd.d/01-custom \
+  && echo "Successfully updated motd"
+
+cp .vimrc ~/.vimrc \
+  && echo "Updated .vimrc"
+
+cp .gitconfig ~/.gitconfig \ 
+  && echo "Updated .gitconfig"
+
+cp .tmux.conf ~/.tmux.conf \
+  && echo "Updated .tmux.conf"
+
