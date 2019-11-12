64837dfef487e19f5cf8e32343610706eeb62b82
Robert <robertchen@live.com>
2019-11-12 18:44:36 +0000

    Updated init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-rm /etc/update-motd.d/* 
-apt-get --assume-yes install screenfetch > /dev/null \
-  && cp 01-custom /etc/update-motd.d/ \
-  && chmod +x /etc/update-motd.d/01-custom \
+sudo rm /etc/update-motd.d/
+sudo apt-get --assume-yes install screenfetch > /dev/null \
+  && sudo cp 01-custom /etc/update-motd.d/ \
+  && sudo chmod +x /etc/update-motd.d/01-custom \
   && echo "Successfully updated motd"
 
 cp .vimrc ~/.vimrc \
@@ -13,7 +13,7 @@ cp .gitconfig ~/.gitconfig \
 cp .tmux.conf ~/.tmux.conf \
   && echo "Updated .tmux.conf"
 
-apt-get --assume-yes install zsh > /dev/null \
+sudo apt-get --assume-yes install zsh > /dev/null \
   && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
   && cp .zshrc ~/.zshrc \
   && echo "Downloaded and setup zsh"
