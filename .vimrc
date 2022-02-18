85e0aafdf7a5a4515528776d6d97c5cb00f700fa
Robert Chen <robertchen@live.com>
2022-02-18 16:36:11 -0500

    update

--- .vimrc
+++ .vimrc
@@ -3,7 +3,12 @@ set tabstop=2
 set softtabstop=2
 set shiftwidth=2
 
-set hlsearch
 set autoindent
+filetype plugin indent on
+syntax on
 
 au BufNewFile,BufRead *.ejs set filetype=html
+
+set hlsearch
+
+autocmd BufWritePre * :%s/\s\+$//e
--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
-sudo rm /etc/update-motd.d/
-sudo apt-get --assume-yes install screenfetch > /dev/null \
-  && sudo cp 01-custom /etc/update-motd.d/ \
-  && sudo chmod +x /etc/update-motd.d/01-custom \
-  && echo "Successfully updated motd"
-
 cp .vimrc ~/.vimrc \
   && echo "Updated .vimrc"
 
@@ -13,11 +7,6 @@ cp .gitconfig ~/.gitconfig \
 cp .tmux.conf ~/.tmux.conf \
   && echo "Updated .tmux.conf"
 
-sudo apt-get --assume-yes install zsh > /dev/null \
-  && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
-  && cp .zshrc ~/.zshrc \
-  && echo "Downloaded and setup zsh"
-
 mkdir -p ~/.config/git \
-  && echo "*.swp" > ~/.config/git/ignore \
+  && echo "*.swp" >> ~/.config/git/ignore \
   && echo "Updated global .gitignore"
